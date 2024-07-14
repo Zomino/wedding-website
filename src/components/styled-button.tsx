1cff71e086c3c536cc6fde02146013a7ac7aac38
Zou Minowa <zou.minowa@shoppermediagroup.com>
2024-07-14 16:31:01 +0100

    AZWW-1 Add styling on hover and active states

--- src/components/styled-button.tsx
+++ src/components/styled-button.tsx
@@ -6,7 +6,7 @@ interface StyledButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement
 
 const StyledButton: React.FC<StyledButtonProps> = ({ children, className, ...props }) => {
     return (
-        <button className={`bg-elephant-darker text-elephant-lighter w-fit p-1 ${className}`} {...props}>
+        <button className={`active:bg-elephant bg-elephant-darker hover:bg-elephant-dark text-elephant-lighter w-fit p-1 ${className}`} {...props}>
             <div className="border border-elephant-lighter py-2 px-5">{children}</div>
         </button>
     )
