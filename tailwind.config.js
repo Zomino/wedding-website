1431ed7ded524df99b0eed3fa5f647ea9e96ef1f
Zou Minowa <zou.minowa@shoppermediagroup.com>
2024-07-14 14:31:33 +0100

    AZWW-1 Add Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,19 @@ module.exports = {
     `./src/components/**/*.{js,jsx,ts,tsx}`,
   ],
   theme: {
+    colors: {
+      elephant: {
+        lighter: '#f5eded',
+        light: '#d4cfcf',
+        DEFAULT: '#a19f9f',
+        dark: '#807e7e',
+        darker: '#676464',
+      },
+    },
+    fontFamily: {
+      raleway: ["Raleway", "sans-serif"],
+      lato: ["Lato", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
