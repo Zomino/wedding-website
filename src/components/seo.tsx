55c0f49c41fe69a9be331f0c25d808768dbcee78
Zou Minowa <zou.minowa@shoppermediagroup.com>
2024-07-15 21:08:31 +0100

    AZWW-3 Make SEO customizable

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -1,7 +1,12 @@
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
-const Seo = () => {
+interface SeoProps {
+    description?: string
+    title?: string
+}
+
+const Seo: React.FC<SeoProps> = ({ description, title }) => {
     const data = useStaticQuery(graphql`
         query {
             site {
@@ -19,8 +24,8 @@ const Seo = () => {
             {/* Gatsby injects these attributes into the body tag */}
             <body className="bg-elephant-lighter font-light text-center text-elephant-darker" />
             <meta name="author" content={data.site.siteMetadata.author} />
-            <meta name="description" content={data.site.siteMetadata.description} />
-            <title>{data.site.siteMetadata.title}</title>
+            <meta name="description" content={description || data.site.siteMetadata.description} />
+            <title>{title ? `${title} | ${data.site.siteMetadata.title}` : data.site.siteMetadata.title}</title>
         </>
     )
 }
