4bb06b9ae6d3f79e834d8c7480ae215f7ea9df28
Zou Minowa <zou.minowa@shoppermediagroup.com>
2024-07-15 22:12:27 +0100

    AWZZ-3 Fix footer to bottom of screen when there is no content

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -9,12 +9,13 @@ interface LayoutProps {
 
 const Layout: React.FC<React.PropsWithChildren<LayoutProps>> = ({ children, pageTitle }) => {
     return (
-        <div className="pt-10">
-            <header className="font-raleway tracking-wider">
+        // Use flexbox with flex-grow on main to make the footer stick to the bottom when the content is shorter than the viewport.
+        <div className="flex flex-col h-screen">
+            <header className="font-raleway pt-10 tracking-wider">
                 <Link className="text-3xl md:text-4xl" to="/">Angela & Zou</Link>
                 <h1 className="mt-5 md:text-xl">{pageTitle}</h1>
             </header>
-            <main className="flex flex-col font-lato max-w-xs min-h-screen mt-5 mx-auto p-5 md:max-w-lg">{children}</main>
+            <main className="flex flex-col flex-grow font-lato max-w-xs mt-5 mx-auto p-5 md:max-w-lg">{children}</main>
             <footer className="font-lato mt-20">
                 <Copyright />
             </footer>
