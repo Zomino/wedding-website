b196ec6fc045ef203f5dc354387f44f228776512
Zou Minowa <zou.minowa@shoppermediagroup.com>
2024-07-20 17:58:41 +0100

    AZWW-25 Update button TS

--- src/components/button-styled-link.tsx
+++ src/components/button-styled-link.tsx
@@ -1,22 +1,24 @@
-import type { GatsbyLinkProps } from "gatsby";
-import { Link } from "gatsby";
-import React from "react";
+import type { GatsbyLinkProps } from 'gatsby';
+import { Link } from 'gatsby';
+import React from 'react';
 
 // Gatsby Link preloads resources for the linked page when the link scrolls into view or when the mouse hovers over it.
 // Gatsby Link ref prop is problematic: https://www.charpeni.com/blog/wrapping-gatsbys-link-with-typescript.
 const ButtonStyledLink = <TState,>(
-  props: React.PropsWithoutRef<GatsbyLinkProps<TState>>
+    props: React.PropsWithoutRef<GatsbyLinkProps<TState>>
 ) => {
-  const { children, className, ...restProps } = props;
+    const { children, className, ...restProps } = props;
 
-  return (
-    <Link
-      className={`active:bg-elephant bg-elephant-darker cursor-pointer font-lato hover:bg-elephant-dark p-1 text-elephant-lighter w-fit ${className}`}
-      {...restProps}
-    >
-      <div className="border border-elephant-lighter py-2 px-5">{children}</div>
-    </Link>
-  );
+    return (
+        <Link
+            className={`w-fit cursor-pointer bg-elephant-darker p-1 font-lato text-elephant-lighter hover:bg-elephant-dark active:bg-elephant ${className}`}
+            {...restProps}
+        >
+            <div className="border border-elephant-lighter px-5 py-2">
+                {children}
+            </div>
+        </Link>
+    );
 };
 
 export default ButtonStyledLink;
