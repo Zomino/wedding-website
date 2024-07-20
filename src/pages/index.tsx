7a40ec3da19e9365aede28ee1e66184747018ef7
Zou Minowa <zou.minowa@shoppermediagroup.com>
2024-07-20 18:00:14 +0100

    AZWW-25 Add form to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import { StaticImage } from 'gatsby-plugin-image';
+import { Button, Input } from '@headlessui/react';
 import React from 'react';
 
 import Layout from '../components/layout';
 import Seo from '../components/seo';
+import StyledButton from '../components/primitives/styled-button';
+import StyledTextInput from '../components/primitives/styled-text-input';
 
 const WelcomePage = () => {
     return (
@@ -13,6 +16,16 @@ const WelcomePage = () => {
                 alt="Drawing of an elephant and a rabbit"
                 className="w-full"
             />
+            <section className="mt-5">
+                <h2 className="sr-only">Authentication Form</h2>
+                <form>
+                    <StyledTextInput
+                        className="m-auto block w-full text-center md:max-w-sm"
+                        placeholder="Enter your unique code"
+                    />
+                    <StyledButton className="mt-4">Enter site</StyledButton>
+                </form>
+            </section>
         </Layout>
     );
 };
