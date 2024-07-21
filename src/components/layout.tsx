2d1b033dc3b9ef3733eed0d4bdd89fd024011876
Zou Minowa <zou.minowa@shoppermediagroup.com>
2024-07-21 13:58:25 +0100

    AZWW-32 Add margin to header

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,9 +5,9 @@ const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
     return (
         // Use flexbox with flex-grow on main to make the footer stick to the bottom when the content is shorter than the viewport.
         <div className="flex min-h-screen flex-col">
-            <header>
+            <header className="mt-10">
                 <Link to="/">
-                    <span className="mt-5 font-raleway text-3xl tracking-widest md:text-4xl lg:text-5xl">
+                    <span className="font-raleway text-3xl tracking-widest md:text-4xl lg:text-5xl">
                         Angela & Zou
                     </span>
                 </Link>
